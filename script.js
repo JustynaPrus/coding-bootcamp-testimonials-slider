7bd69b3f5b7715d3ec7729c07c9746d0d75a4a1e
Justyna Pruś <65462428+JustynaPrus@users.noreply.github.com>
2021-10-03 20:54:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const sectionTanya = document.querySelector('.tanya');
 const sectionJohn = document.querySelector('.john');
 const bar = document.querySelector('.bar');
-
-sectionTanya.classList.add('active');
+bar.classList.add('red');
 
 bar.addEventListener('click', sectionChange);
 
 function sectionChange() {
-    sectionTanya.classList.toggle('active');
-    sectionJohn.classList.toggle('active');
+    if (sectionTanya.className === 'tanya active') {
+        sectionTanya.classList.remove('active');
+        sectionJohn.classList.add('active');
+    } else {
+        sectionJohn.classList.remove('active');
+        sectionTanya.classList.add('active');
+    }
 }
